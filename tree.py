2981e9036fa1e81f8295bf29a8780e2cbe804ba1
Michael Shuff <mjshuff23@gmail.com>
2020-11-05 15:41:55 -0500

    fixed recursion

--- tree.py
+++ tree.py
@@ -4,6 +4,11 @@ class Node:
         self._parent = None
         self._children = []
 
+    # def __repr__(self):
+    #     print(
+    #         f'Node val: {self.value}, par: {self.parent}'
+    #     )
+
     @property
     def value(self):
         return self._value
@@ -19,37 +24,35 @@ class Node:
     @parent.setter
     def parent(self, node):
         if not node:
+            self._parent = None
             return
-        if self._parent:
-            print(self._parent)
-            self.parent.remove_child(self)
+        if self._parent != node:
+            if self._parent != None:
+                self.parent.remove_child(self)
         self._parent = node
         node.add_child(self)
 
     def add_child(self, node):
-        print('pre-count', self._children.count(node))
         if self._children.count(node) == 0:
-            print('postcount', self._children.count(node))
             self._children.append(node)
             node.parent = self
 
     def remove_child(self, node):
         try:
-            print('pre-removal', self._children)
             self._children.remove(node)
             node.parent = None
         except:
             print('Node does not exist')
 
 
-node1 = Node("root1")
-node2 = Node("root2")
-node3 = Node("root3")
+# node1 = Node("root1")
+# node2 = Node("root2")
+# node3 = Node("root3")
 
-node3.parent = node1
+# node3.parent = node1
 # node3.parent = node2
 # node1.add_child(node3)
 # node1.add_child(node3)
 
-print(node1.children)
-print(node2.children)
+# print(node1.children)
+# print(node2.children)
