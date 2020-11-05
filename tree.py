1ea1728b3075bf01d98f3497f3aea46df5287151
Xander <emili.xander@gmail.com>
2020-11-05 10:16:09 -0900

    recursion rears its ugly head

--- tree.py
+++ tree.py
@@ -20,26 +20,36 @@ class Node:
     def parent(self, node):
         if not node:
             return
+        if self._parent:
+            print(self._parent)
+            self.parent.remove_child(self)
         self._parent = node
-        node.add_child = self
+        node.add_child(self)
 
     def add_child(self, node):
+        print('pre-count', self._children.count(node))
         if self._children.count(node) == 0:
+            print('postcount', self._children.count(node))
             self._children.append(node)
             node.parent = self
 
     def remove_child(self, node):
         try:
+            print('pre-removal', self._children)
             self._children.remove(node)
             node.parent = None
         except:
             print('Node does not exist')
 
 
-newNode = Node('hey')
-newNode2 = Node('ho')
-print(newNode.value)
-newNode.add_child(newNode2)
-print(newNode.children)
-newNode.add_child(newNode2)
-print(newNode.children)
+node1 = Node("root1")
+node2 = Node("root2")
+node3 = Node("root3")
+
+node3.parent = node1
+# node3.parent = node2
+# node1.add_child(node3)
+# node1.add_child(node3)
+
+print(node1.children)
+print(node2.children)
